e65a788dc466fb14f9bb1d89842749170b0f3b94
jotertu <joaotertuliano00@gmail.com>
2023-10-17 17:36:00 -0300

    feat: Finalizado projeto farmacia

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,33 +1,28 @@
-import React from 'react'
-import { Link, } from 'react-router-dom'
-import logo from '../../assets/logo.png'
-import user from '../../assets/user.png'
-import car from '../../assets/carrinho.png'
+import { Link } from 'react-router-dom';
+import carrinho from "../../assets/carrinho.png";
+import user from "../../assets/user.png";
+
 
 
 function Navbar() {
-    return (
-        <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-          <div className="container flex items-center justify-between text-lg">
-            <div className='max-w-xs max-h-20'> <img src={logo} alt='' className=''/> </div>
 
-            <div className='flex'>
+  return (
+     <div className='w-full bg-blue-900 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+            <Link to='/' className='text-2xl font-bold uppercase text-white'>Farmacia</Link>
             <label className="relative block w-96">
-                  <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Pesquisar" type="text" name="search"/>
-                  </label>
-            </div>
+                <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 text-black rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Pesquisar" type="text" name="search"/>
+              </label>
 
-            <div className='flex  gap-4'>
-              <div  className='hover:underline'>Categorias</div>
-              <div  className='hover:underline'>Cadastrar categoria</div>
-              <div  className='hover:underline'><img src={user} alt='' className='w-3/4'/></div>
-              <div  className='hover:underline'><img src={car} alt='' className='w-3/4'/></div>
+            <div className='flex gap-4'>
+              <Link to='/categories' className='hover:underline text-white'>Categorias</Link>
+              <Link to='/cadastroCategory' className='hover:underline text-white'>Cadastrar Categoria</Link>
+              <img src={carrinho} alt="" className="imagem" />
+              <img src={user} alt="" className="imagem" />
             </div>
           </div>
         </div>
-      )
-    }
-
-
+  )
+}
 
 export default Navbar
